b5e522eb8fe6034a7d9b09cc0cbb9c1783b517c6
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-10 14:05:26 +0900

    1004

--- baekjoon/1004.cpp
+++ baekjoon/1004.cpp
@@ -1,24 +1,24 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int sqrt(int a, int b) {
-	return a * a + b * b;
-}
-int main() {
-	int T, x1, y1, x2, y2, n, x, y, r, cnt=0;
-	scanf("%d", &T);
-	for(int j=0;j<T;j++){
-		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-		scanf("%d", &n);
-		for (int i = 0; i < n; i++) {
-			scanf("%d %d %d", &x, &y, &r);
-			if ((sqrt(x - x1, y - y1) < r * r) && (sqrt(x - x2, y - y2) > r * r)) {
-				cnt++;
-			}
-			else if ((sqrt(x - x1, y - y1) > r * r) && (sqrt(x - x2, y - y2) < r * r)) {
-				cnt++;
-			}
-		}
-		printf("%d\n", cnt);
-		cnt = 0;
-	}
-}
+//#include<stdio.h>
+//int sqrt(int a, int b) {
+//	return a * a + b * b;
+//}
+//int main() {
+//	int T, x1, y1, x2, y2, n, x, y, r, cnt=0;
+//	scanf("%d", &T);
+//	for(int j=0;j<T;j++){
+//		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+//		scanf("%d", &n);
+//		for (int i = 0; i < n; i++) {
+//			scanf("%d %d %d", &x, &y, &r);
+//			if ((sqrt(x - x1, y - y1) < r * r) && (sqrt(x - x2, y - y2) > r * r)) {
+//				cnt++;
+//			}
+//			else if ((sqrt(x - x1, y - y1) > r * r) && (sqrt(x - x2, y - y2) < r * r)) {
+//				cnt++;
+//			}
+//		}
+//		printf("%d\n", cnt);
+//		cnt = 0;
+//	}
+//	return 0;
+//}
