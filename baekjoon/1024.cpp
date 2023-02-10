17af1392397da7e5f4ecb3b2a534c77e84ffd521
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-10 23:40:18 +0900

    1024 clear

--- baekjoon/1024.cpp
+++ baekjoon/1024.cpp
@@ -1,24 +1,24 @@
-#include<stdio.h>
-int main() {
-	int x, n, l;
-	scanf("%d %d", &n, &l);
-	
-	for (int i = l; i <= 100; i++) {
-		x = n;
-		x *= 2;
-		if (x % i == 0) {
-			x /= i;
-			if(x+1-i>=0){
-				x += 1 - i;
-				if (x % 2 == 0) {
-					x /= 2;
-					for (int k = 0; k < i; k++) {
-						printf("%d ", x++);
-					}
-					break;
-				}
-			}
-		}
-		if (i == 100) printf("-1");
-	}
-}
+//#include<stdio.h>
+//int main() {
+//	int x, n, l;
+//	scanf("%d %d", &n, &l);
+//	
+//	for (int i = l; i <= 100; i++) {
+//		x = n;
+//		x *= 2;
+//		if (x % i == 0) {
+//			x /= i;
+//			if(x+1-i>=0){
+//				x += 1 - i;
+//				if (x % 2 == 0) {
+//					x /= 2;
+//					for (int k = 0; k < i; k++) {
+//						printf("%d ", x++);
+//					}
+//					break;
+//				}
+//			}
+//		}
+//		if (i == 100) printf("-1");
+//	}
+//}
