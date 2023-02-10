0c5f39b0240bc04ec565d5e55b31972edab1fd3c
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2023-02-10 20:13:04 +0900

    1012 clear

--- baekjoon/1012.cpp
+++ baekjoon/1012.cpp
@@ -1,29 +1,35 @@
-#include<stdio.h>
-int main() {
-	int t, m, n, k;
-	int x, y;
-	scanf("%d", &t);
-	for (int i = 0; i < t; i++) {
-		int cnt = 0;
-		int area[52][52] = { 0 };
-		int check[52][52] = { 0 };
-		scanf("%d %d %d", &m, &n, &k);
-		for (int j = 0; j < k; j++) {
-			scanf("%d %d", &x, &y);
-			area[x+1][y+1] = 1;
-		}
-		for (int a = 1; a <= m; a++) {
-			for (int b = 1; b <= n; b++) {
-				if (area[a][b] == 1) {
-					check[a][b] = 1;
-					if (check[a + 1][b] == 1) continue;
-					else if (check[a - 1][b] == 1) continue;
-					else if (check[a][b + 1] == 1) continue;
-					else if (check[a][b - 1] == 1) continue;
-					else cnt++;
-				}
-			}
-		}
-		printf("%d\n", cnt);
-	}
-}
+//#include<stdio.h>
+//int m, n;
+//int area[52][52] = { 0 };
+//int cabbage(int a, int b) {
+//	area[a][b] = 0;
+//	if (area[a - 1][b] == 1) cabbage(a - 1, b);
+//	if (area[a + 1][b] == 1) cabbage(a + 1, b);
+//	if (area[a][b-1] == 1) cabbage(a, b-1);
+//	if (area[a][b+1] == 1) cabbage(a, b+1);
+//	return 0;
+//}
+//int main() {
+//	int t, k;
+//	int x, y;
+//	scanf("%d", &t);
+//	for (int i = 0; i < t; i++) {
+//		int cnt = 0;
+//		scanf("%d %d %d", &m, &n, &k);
+//		for (int j = 0; j < k; j++) {
+//			scanf("%d %d", &x, &y);
+//			area[x+1][y+1] = 1;
+//		}
+//		for (int a = 1; a <= m; a++) {
+//			for (int b = 1; b <= n; b++) {
+//				if (area[a][b] == 1) {
+//					cabbage(a, b);
+//					cnt++;
+//				}
+//			}
+//		}
+//		printf("%d\n", cnt);
+//		m = 0;
+//		n = 0;
+//	}
+//}
