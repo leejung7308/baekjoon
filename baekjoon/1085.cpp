45beff35238b43848f89df48a178822bcf77a72b
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-12 18:12:10 +0900

    1085 clear

---
+++ baekjoon/1085.cpp
@@ -0,0 +1,9 @@
+//#include<stdio.h>
+//int main() {
+//	int x, y, w, h;
+//	scanf("%d %d %d %d", &x, &y, &w, &h);
+//	int minx, miny;
+//	w - x <= x ? minx = w - x : minx = x;
+//	h - y <= y ? miny = h - y : miny = y;
+//	minx <= miny ? printf("%d", minx) : printf("%d", miny);
+//}
