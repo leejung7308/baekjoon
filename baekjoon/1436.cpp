98662cb3303dd55ca00b1a8601a4e6c50f15d544
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-12 22:04:06 +0900

    1436 clear

---
+++ baekjoon/1436.cpp
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//int main() {
+//	int n, res = 666, cnt = 1;
+//	scanf("%d", &n);
+//	if (n == 1) {
+//		printf("%d", res);
+//		return 0;
+//	}
+//	while (cnt != n) {
+//		res++;
+//		int tmp = res;
+//		while (tmp != 0) {
+//			if (tmp % 1000 == 666) {
+//				cnt++;
+//				break;
+//			}
+//			else tmp /= 10;
+//		}
+//	}
+//	printf("%d", res);
+//}
