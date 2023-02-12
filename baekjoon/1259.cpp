5e9c05214d6a9d9e154388d7f9ea8116009bae88
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-12 20:53:17 +0900

    1259 clear

---
+++ baekjoon/1259.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main() {
+//	char a[6];
+//	while (1) {
+//		scanf("%s", a);
+//		int j = strlen(a)-1;
+//		if (a[0]-'0' == 0) return 0;
+//		for (int i = 0; i < strlen(a); i++) {
+//			if (a[i] != a[j]) {
+//				printf("no\n");
+//				break;
+//			}
+//			if (i == strlen(a) - 1) printf("yes\n");
+//			j--;
+//		}
+//	}
+//}
