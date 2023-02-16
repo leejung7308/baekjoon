39d32b772e3ae6d838404302aee9558cc790d703
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-16 16:59:38 +0900

    2609 clear

---
+++ baekjoon/2609.cpp
@@ -0,0 +1,11 @@
+//#include<stdio.h>
+//int GCD(int a, int b) {
+//	if (a % b == 0) return b;
+//	else return GCD(b, a % b);
+//}
+//int main() {
+//	int a, b, gcd;
+//	scanf("%d %d", &a, &b);
+//	gcd = GCD(a, b);
+//	printf("%d\n%d", gcd,a*b/gcd);
+//}
