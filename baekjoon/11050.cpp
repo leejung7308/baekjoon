7a21036031af787a46721f6061e613bbf4307179
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-18 19:38:07 +0900

    11050 clear

---
+++ baekjoon/11050.cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main() {
+//	int n, k;
+//	scanf("%d %d", &n, &k);
+//	if (n - k < k) k = n - k;
+//	int res = 1;
+//	for (int i = 1; i <= k; i++)
+//	{
+//		res *= n--;
+//		res /= i;
+//	}
+//	printf("%d", res);
+//}
