817986da6c8eb90d911265eee6a0267bfcd24aef
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-18 15:10:22 +0900

    9012 clear

---
+++ baekjoon/9012.cpp
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main() {
+//	char a[50];
+//	int t, flag=0;
+//	scanf("%d", &t);
+//	for (int i = 0; i < t; i++)
+//	{
+//		scanf("%s", a);
+//		int len = strlen(a);
+//		for (int j = 0; j < len; j++)
+//		{
+//			if (a[j] == '(') flag++;
+//			else flag--;
+//			if (flag < 0) {
+//				flag = 1;
+//				break;
+//			}
+//		}
+//		if (flag) printf("NO\n");
+//		else printf("YES\n");
+//		flag = 0;
+//	}
+//}
