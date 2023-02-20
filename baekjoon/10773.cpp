b88cc82d8430dafb1f7da7473d64fdfc719c5edf
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-20 20:44:36 +0900

    10773 clear

---
+++ baekjoon/10773.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int k,sum=0,index=0;
+//	scanf("%d", &k);
+//	int* a = (int*)malloc(sizeof(int) * k);
+//	while(k--)
+//	{
+//		scanf("%d", &a[index]);
+//		if (a[index] == 0) {
+//			sum -= a[index - 1];
+//			index--;
+//		}
+//		else sum += a[index++];
+//	}
+//	printf("%d", sum);
+//}
