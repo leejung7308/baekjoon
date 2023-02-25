0212b682d7980ae885b72c3f3f1c693b7711a0c5
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-25 17:22:37 +0900

    1009 clear

---
+++ baekjoon/1009.cpp
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main() {
+//	int t, a, b;
+//	scanf("%d", &t);
+//	for (int i = 0; i < t; i++)
+//	{
+//		int tmp = 1;
+//		scanf("%d %d", &a, &b);
+//		for (int j = 0; j < b; j++)
+//		{
+//			tmp = tmp * a % 10;
+//		}
+//		if (tmp == 0) tmp = 10;
+//		printf("%d\n", tmp);
+//	}
+//}
