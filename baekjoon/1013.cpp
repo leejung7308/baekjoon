539a640cd4990159da467e3db160d6303a089b53
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-26 10:51:06 +0900

    1013 주석처리

---
+++ baekjoon/1013.cpp
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//#include<math.h>
+//int main() {
+//	int t, cnt = 0;
+//	char a[201] = { 0 };
+//	int res = 1;
+//	int flag1 = 0, flag2 = 0;
+//	for (int i = 0; i < t; i++)
+//	{
+//		scanf("%s", a);
+//		for (int j = 0; j < strlen(a); j++) a[j] - 48;
+//		if (a[0] == 1) {
+//			for (int j = 1; j < strlen(a); j++)
+//			{
+//				if (cnt > 3) {
+//					res = 0;
+//					break;
+//				}
+//				if (a[j] == 0) cnt++;
+//				else if (a[j] == 1) cnt = 0;
+//			}
+//		}
+//		
+//	}
+//}
