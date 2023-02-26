42184fbb3a9915174049b404a9cdc62ba919f8c5
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-26 10:58:09 +0900

    1032 clear

---
+++ baekjoon/1032.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main() {
+//	int t;
+//	char arr[100], tmp[100];
+//	scanf("%d", &t);
+//	scanf("%s", arr);
+//	for (int i = 0; i < t-1; i++)
+//	{
+//		scanf("%s", tmp);
+//		for (int j = 0; j < strlen(arr); j++)
+//		{
+//			if (arr[j] != tmp[j])arr[j] = '?';
+//		}
+//	}
+//	printf("%s", arr);
+//}
