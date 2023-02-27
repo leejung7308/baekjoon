ad98f0066c545cf43530c0f999a57061accb73b5
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-27 23:29:24 +0900

    1037 clear

---
+++ baekjoon/1037.cpp
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int cmp(const void* a, const void* b) {
+//	return *(int*)a - *(int*)b;
+//}
+//int main() {
+//	int n;
+//	scanf("%d", &n);
+//	int* arr = (int*)malloc(sizeof(int)*n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	qsort(arr, n, sizeof(int), cmp);
+//	printf("%d", arr[0] * arr[n - 1]);
+//}
