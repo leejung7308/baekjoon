e6415cf95ff59bf841d8291cadc012ad0074f3c6
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-02-28 21:15:12 +0900

    1075 clear

---
+++ baekjoon/1075.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//int main() {
+//	int n, f;
+//	scanf("%d %d", &n, &f);
+//	if (n >= 100) {
+//		n = n / 100;
+//		n *= 100;
+//	}
+//	for (int i = 0; i < 100; i++)
+//	{
+//		if ((n + i) % f == 0) {
+//			if (i < 10) printf("0%d", i);
+//			else printf("%d", i);
+//			break;
+//		}
+//	}
+//}
