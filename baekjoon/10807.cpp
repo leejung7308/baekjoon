7e9407838d7d410cd45b46682c8be8426ddc2365
leekkzzzz@soongsil.ac.kr <leekkzzzz@soongsil.ac.kr>
2023-03-07 22:33:50 +0900

    10807 clear

---
+++ baekjoon/10807.cpp
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//int main() {
+//	int n, arr[100] = { 0 };
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int tmp, cnt = 0;
+//	scanf("%d", &tmp);
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (arr[i] == tmp) cnt++;
+//	}
+//	printf("%d", cnt);
+//}
