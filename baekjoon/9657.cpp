a0593732151ff3f8d6bd56e9b2c27ed61225f00c
leejung7308 <leekkzzzz@naver.com>
2024-09-07 19:52:42 +0900

    9657 clear

---
+++ baekjoon/9657.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main() {
+//	int N;
+//	cin >> N;
+//	int a, b, c;
+//	a = N - 1;
+//	b = N - 3;
+//	c = N - 4;
+//	if (a % 7 == 0 || a % 7 == 2)
+//		cout << "SK";
+//	else if (b % 7 == 0 || b % 7 == 2)
+//		cout << "SK";
+//	else if (c % 7 == 0 || c % 7 == 2)
+//		cout << "SK";
+//	else
+//		cout << "CY";
+//}
