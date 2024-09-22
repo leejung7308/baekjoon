41f1808f2945966206983f40391a1744be222126
leejung7308 <leekkzzzz@naver.com>
2024-09-22 15:09:13 +0900

    9658 clear

---
+++ baekjoon/9658.cpp
@@ -0,0 +1,10 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main() {
+//	int n;
+//	cin >> n;
+//	if ((n - 2) % 7 == 0 || (n - 2) % 7 == 2) cout << "SK";
+//	else if ((n - 4) % 7 == 0 || (n - 4) % 7 == 2) cout << "SK";
+//	else if ((n - 5) % 7 == 0 || (n - 5) % 7 == 2) cout << "SK";
+//	else cout << "CY";
+//}
