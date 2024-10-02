74eb42b17b2446b673d92628d64d61ab4099e48c
leejung7308 <leekkzzzz@naver.com>
2024-10-02 17:04:06 +0900

    1105 clear

---
+++ baekjoon/1105.cpp
@@ -0,0 +1,19 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	string l, r;
+//	int cnt = 0;
+//	cin >> l >> r;
+//	if (l.length() != r.length()) {
+//		cout << "0" << endl;
+//		return 0;
+//	}
+//	for (int i = 0; i < l.length(); i++) {
+//		if (l[i] == r[i] && l[i] == '8') cnt++;
+//		if(l[i] != r[i]) break;
+//	}
+//	cout << cnt << endl;
+//}
