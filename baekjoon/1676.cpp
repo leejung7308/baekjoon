efb1852815a6fa1602ba791081b3d3163ad066f0
leejung7308 <leekkzzzz@naver.com>
2024-10-21 18:03:52 +0900

    1676 clear

---
+++ baekjoon/1676.cpp
@@ -0,0 +1,19 @@
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n, ans = 0;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		if (i % 5 == 0) {
+//			int tmp = i;
+//			while (tmp % 5 == 0) {
+//				tmp /= 5;
+//				ans++;
+//			}
+//		}
+//	}
+//	cout << ans;
+//}
