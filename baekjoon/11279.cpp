177bc4dea0a38ddfefc7115e0a8d74f775fbf46b
leejung7308 <leekkzzzz@naver.com>
2024-11-06 12:03:50 +0900

    11279 clear

---
+++ baekjoon/11279.cpp
@@ -0,0 +1,23 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n;
+//	cin >> n;
+//	priority_queue<long long> pq;
+//	for (int i = 0; i < n; i++) {
+//		long long op;
+//		cin >> op;
+//		if (op) pq.push(op);
+//		else {
+//			if (pq.size() == 0) cout << 0 << '\n';
+//			else {
+//				cout << pq.top()<<'\n';
+//				pq.pop();
+//			}
+//		}
+//	}
+//}
