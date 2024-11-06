c96d70bfae89fba52cc399d589019221b8e3dc4d
leejung7308 <leekkzzzz@naver.com>
2024-11-06 11:38:45 +0900

    18870 clear

---
+++ baekjoon/18870.cpp
@@ -0,0 +1,20 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n;
+//	cin >> n;
+//	vector<int> v(n), c(n);
+//	for (int i = 0; i < n; i++) {
+//		cin >> v[i];
+//		c[i] = v[i];
+//	}
+//	sort(c.begin(), c.end());
+//	c.erase(unique(c.begin(), c.end()), c.end());
+//	for (int i = 0; i < n; i++) {
+//		cout << lower_bound(c.begin(), c.end(), v[i]) - c.begin() << ' ';
+//	}
+//}
