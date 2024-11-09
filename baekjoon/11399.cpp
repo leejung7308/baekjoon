08390c94fa47fdd735481c21d256d650091d7da7
leejung7308 <leekkzzzz@naver.com>
2024-11-09 18:28:17 +0900

    11399 clear

---
+++ baekjoon/11399.cpp
@@ -0,0 +1,24 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n;
+//	cin >> n;
+//	vector<int>line;
+//
+//	for (int i = 0; i < n; i++) {
+//		int k;
+//		cin >> k;
+//		line.push_back(k);
+//	}
+//	sort(line.begin(), line.end());
+//
+//	int answer = 0;
+//	for (int i = 0; i < n; i++) {
+//		answer += line[i] * (n - i);
+//	}
+//	cout << answer;
+//}
