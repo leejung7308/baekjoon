eb6f4a287481d4c3b965ff4d94e92ce70c9ce744
leejung7308 <leekkzzzz@naver.com>
2024-11-12 16:43:10 +0900

    11659 clear

---
+++ baekjoon/11659.cpp
@@ -0,0 +1,21 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n, m;
+//	cin >> n >> m;
+//	vector<int> tmp(n + 1);
+//	for (int i = 1; i < n+1; i++) {
+//		int a;
+//		cin >> a;
+//		tmp[i] = tmp[i - 1] + a;
+//	}
+//	for (int i = 0; i < m; i++) {
+//		int a, b;
+//		cin >> a >> b;
+//		cout << tmp[b] - tmp[a - 1] << '\n';
+//	}
+//}
