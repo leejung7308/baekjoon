2cd55c4fbd6345680d77911e0f410db5a032adee
leejung7308 <leekkzzzz@naver.com>
2024-11-13 10:29:08 +0900

    17219 clear

---
+++ baekjoon/17219.cpp
@@ -0,0 +1,22 @@
+//#include<bits/stdc++.h>
+//#include<unordered_map>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n, m;
+//	cin >> n >> m;
+//	unordered_map<string, string> pw;
+//	for (int i = 0; i < n; i++) {
+//		string site, psw;
+//		cin >> site >> psw;
+//		pw[site] = psw;
+//	}
+//	for (int i = 0; i < m; i++) {
+//		string site;
+//		cin >> site;
+//		cout << pw[site] << '\n';
+//	}
+//}
