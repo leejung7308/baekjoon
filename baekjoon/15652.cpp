8270695a8e751cb06f8e991dd00be3131821a125
leejung7308 <leekkzzzz@naver.com>
2024-11-16 11:28:10 +0900

    15652 clear

---
+++ baekjoon/15652.cpp
@@ -0,0 +1,28 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int n, m;
+//vector<int> v;
+//void brute(int cnt, int prev) {
+//	if (cnt == m) 
+//	{
+//		for(int i=0;i<v.size();i++)
+//		{
+//			cout << v[i] << ' ';
+//		}
+//		cout << '\n';
+//		return;
+//	}
+//	for (int i = prev; i <= n; i++) {
+//		v.push_back(i);
+//		brute(cnt + 1, i);
+//		v.pop_back();
+//	}
+//}
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	cin >> n >> m;
+//	brute(0, 1);
+//}
