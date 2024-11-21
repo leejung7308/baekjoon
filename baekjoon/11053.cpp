a05426dbb6b373beadd526e1fc4be95606324882
leejung7308 <leekkzzzz@naver.com>
2024-11-21 13:33:37 +0900

    11053 clear

---
+++ baekjoon/11053.cpp
@@ -0,0 +1,20 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int n;
+//	cin >> n;
+//	vector<int> a(n);
+//	vector<int> d(n, INF);
+//	for (int i = 0; i < n; i++) {
+//		cin >> a[i];
+//	}
+//	for (int i = 0; i < n; i++) {
+//		*lower_bound(d.begin(), d.end(), a[i]) = a[i];
+//	}
+//	cout << lower_bound(d.begin(), d.end(), INF) - d.begin();
+//	return 0;
+//}
