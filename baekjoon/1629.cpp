968d410ec581fb55c628ef93ad75454ec070f21e
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-11-27 12:49:36 +0900

    1629 clear

---
+++ baekjoon/1629.cpp
@@ -0,0 +1,18 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//long long a, b, c, k;
+//long long power(long long b) {
+//	if (b == 0) return 1;
+//	if (b == 1) return a % c;
+//	k = power(b / 2) % c;
+//	if (b % 2 == 0) return k * k % c;
+//	else return k * k % c * a % c;
+//}
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	cin >> a >> b >> c;
+//	cout << power(b);
+//}
