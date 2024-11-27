18e1e661d01c89f60c2a3390ff5ad805c0e277d8
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-11-27 11:36:44 +0900

    16953 clear

---
+++ baekjoon/16953.cpp
@@ -0,0 +1,18 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int a, b, cnt=1;
+//	cin >> a >> b;
+//	while (b > a) {
+//		if (b % 2 == 0) b /= 2;
+//		else if (b % 10 == 1) b /= 10;
+//		else break;
+//		cnt++;
+//	}
+//	if (a == b) cout << cnt;
+//	else cout << -1;
+//}
