febd97ce30d15d786ca3fc9e01ebb2b7eb1d7ede
leejung7308 <leekkzzzz@naver.com>
2024-12-02 21:59:19 +0900

    Programmer Lv2 점찍기 clear

---
+++ baekjoon/점찍기.cpp
@@ -0,0 +1,17 @@
+//#include <string>
+//#include <vector>
+//#include <cmath>
+//using namespace std;
+//
+//long long solution(int k, int d) {
+//    long long answer = 0;
+//    long long y;
+//    long long x;
+//    for (x = 0; x <= d; x += k) {
+//        y = sqrt((long long)d * d - x * x);
+//        answer += y / k + 1;
+//    }
+//
+//
+//    return answer;
+//}
