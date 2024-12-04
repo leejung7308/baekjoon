f0861a616c468b629ef7f145e2d623c2dabfe5af
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-12-04 12:30:18 +0900

    Programmers Lv2 올바른괄호 clear

---
+++ baekjoon/올바른괄호.cpp
@@ -0,0 +1,19 @@
+//#include<string>
+//#include <iostream>
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//bool solution(string s)
+//{
+//    bool answer = true;
+//    int o = 0, d = 0;
+//    for (auto& c : s) {
+//        if (c == '(') o++;
+//        else if (c == ')') {
+//            if (++d > o) return false;
+//        }
+//    }
+//    if (o != d)return false;
+//
+//    return answer;
+//}
