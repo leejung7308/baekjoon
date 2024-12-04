3906db2e76a3157c095cbc7f32c5d3cb6f33f5f5
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-12-04 12:30:09 +0900

    Programmers Lv2 JadenCase문자열만들기 clear

---
+++ baekjoon/JadenCase문자열만들기.cpp
@@ -0,0 +1,14 @@
+//#include <string>
+//#include <vector>
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//string solution(string s) {
+//    string answer = "";
+//    answer.push_back(toupper(s[0]));
+//    for (int i = 1; i < s.size(); i++) {
+//        if (s[i - 1] != ' ') answer.push_back(tolower(s[i]));
+//        else answer.push_back(toupper(s[i]));
+//    }
+//    return answer;
+//}
