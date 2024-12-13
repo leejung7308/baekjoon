eaeef852c4eaa9738b8de6095bb8e7ecaa0a0c17
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-12-13 13:09:59 +0900

    2744 clear

---
+++ baekjoon/2744.cpp
@@ -0,0 +1,14 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	string s;
+//	cin >> s;
+//	for (auto& c : s) {
+//		if (c >= 97) cout << (char)toupper(c);
+//		else cout << (char)tolower(c);
+//	}
+//}
