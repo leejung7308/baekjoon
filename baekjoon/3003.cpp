51106a84038266a8afdeaf5d4a51b2c5055d7045
leejung7308 <17855133+leejung7308@users.noreply.github.com>
2024-12-15 14:21:15 +0900

    3003 clear

---
+++ baekjoon/3003.cpp
@@ -0,0 +1,17 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	vector<int> t,v = { 1,1,2,2,2,8 };
+//	for (auto& i : v) {
+//		int tmp;
+//		cin >> tmp;
+//		t.push_back(i - tmp);
+//	}
+//	for(auto& i:t){
+//		cout << i << ' ';
+//	}
+//}
