69a396e3d1ebce412f3030c59ab334ffe2e5273d
leejung7308 <leekkzzzz@naver.com>
2024-12-18 09:25:03 +0900

    4999 clear

---
+++ baekjoon/4999.cpp
@@ -0,0 +1,12 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	string a, b;
+//	cin >> a >> b;
+//	if (a.size() >= b.size()) cout << "go";
+//	else cout << "no";
+//}
