6f8f34da324649690ea36193a0ba3c2bf8ac5d31
leejung7308 <leekkzzzz@naver.com>
2024-12-22 14:14:50 +0900

    5522 clear

---
+++ baekjoon/5522.cpp
@@ -0,0 +1,15 @@
+//#include<bits/stdc++.h>
+//#define INF 987654321
+//using namespace std;
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	cout.tie(0);
+//	int ans = 0;
+//	int tmp;
+//	for (int i = 0; i < 5; i++) {
+//		cin >> tmp;
+//		ans += tmp;
+//	}
+//	cout << ans;
+//}
