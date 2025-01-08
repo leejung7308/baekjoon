882090aab514bc9e1e383affee82a0c4d9877b1d
Jung <leekkzzzz@naver.com>
2025-01-08 14:42:56 +0900

    [Gold V] Title: 강의실 배정, Time: 56 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,27 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define INF 987654321
 using namespace std;
-
-int main(){
-    int n, tmp1, tmp2;
-    vector<pair<int, int>> st;
-    priority_queue<int, vector<int>, greater<int>> t;
-    
-    cin >> n;
-    
-    while(n--){
-        cin >> tmp1 >> tmp2;
-        st.push_back(make_pair(tmp1, tmp2));
-    }
-    
-    sort(st.begin(), st.end());
-    
-    for(int i = 0; i < st.size(); i++){
-        t.push(st[i].second);
-        if(t.top() <= st[i].first) t.pop();
-    }
-    
-    cout << t.size();
-    
-    
-    return 0;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	priority_queue<int> pq;
+	vector<pair<int, int>> v;
+	while (n--) {
+		int s, t;
+		cin >> s >> t;
+		v.push_back({ s,t });
+	}
+	sort(v.begin(), v.end());
+	for(auto& i:v) {
+		pq.push(-i.second);
+		if (-pq.top() <= i.first) pq.pop();
+	}
+	cout << pq.size();
 }
