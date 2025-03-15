4a427759c45b7afdb66318efde945f444e85785f
Jung <leekkzzzz@naver.com>
2025-03-15 15:03:56 +0900

    [Gold V] Title: 잠수함식별, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.cc
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.cc
@@ -1,9 +1,38 @@
-#include <iostream>
-#include <regex>
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define INF 1000000000
+#define FAST_IO ios::sync_with_stdio; cin.tie(0); cout.tie(0)
 using namespace std;
-
 int main() {
+	FAST_IO;
 	string s;
 	cin >> s;
-	cout << (regex_match(s, regex("(100+1+|01)+")) ? "SUBMARINE": "NOISE") << endl;
+	bool flag = true;
+	int cur = 0;
+	if (s[s.size() - 1] == '0') {
+		cout << "NOISE";
+		return 0;
+	}
+	while (flag && cur < s.size()) {
+		if (s[cur] == '1') { //1
+			int zcnt = 0, ocnt = 0;
+			cur++;
+			while (cur < s.size() && s[cur] == '0') zcnt++, cur++;
+			if (zcnt < 2) flag = false;
+			else {
+				while (cur < s.size() && s[cur] == '1') ocnt++, cur++;
+				if (cur + 1 < s.size() && s[cur + 1] == '1') cur += 2;
+				else if (cur + 1 < s.size() && s[cur + 1] == '0') {
+					if (ocnt == 1) flag = false;
+					cur--;
+				}
+			}
+		}
+		else {//0
+			if (cur + 1 < s.size() && s[cur + 1] == '1') cur += 2;
+			else flag = false;
+		}
+	}
+	if (flag) cout << "SUBMARINE";
+	else cout << "NOISE";
 }
