ec63313f44e9c70e4fdc05233140d510a0033a7c
Jung <leekkzzzz@naver.com>
2025-04-10 20:30:43 +0900

    [level 2] Title: 삼각 달팽이, Time: 168.90 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -4,20 +4,25 @@
 using namespace std;
 
 vector<int> solution(int n) {
-    vector<int> answer;
-    vector<vector<int>> v(n,vector<int>(n));
-    vector<pair<int,int>> dir = {{1,0},{0,1},{-1,-1}};
-    int cy=-1, cx=0, flag=0, num=1;
+    vector<int> answer(n*(n+1)/2);
+    int index=0, skip=0, flag=0, num=1;
     for(int i=n;i>0;i--){
         for(int j=i;j>0;j--){
-            cy+=dir[flag].first;
-            cx+=dir[flag].second;
-            v[cy][cx] = num++;
+            switch(flag){
+                case 0:
+                    index+=skip++;
+                    answer[index]=num++;
+                    break;
+                case 1:
+                    answer[++index]=num++;
+                    break;
+                case 2:
+                    index-=skip--;
+                    answer[index]=num++;
+                    break;
+            }
         }
         ++flag %= 3;
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0; j<i+1;j++) answer.push_back(v[i][j]);
     }
     return answer;
 }
