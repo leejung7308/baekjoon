cc84f64954216ac44b1625c5e4214c8b364203a2
Jung <leekkzzzz@naver.com>
2025-04-11 19:07:21 +0900

    [Platinum V] Title: 행성 터널, Time: 220 ms, Memory: 13720 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -14,16 +14,16 @@ void uni(int a, int b) {
 	b = findp(b);
 	a < b ? parent[b] = a : parent[a] = b;
 }
-struct pos {int x, y, z;};
+struct pos {int x, y, z, n;};
 struct weight { int w, a, b;};
-bool compx(const pair<int, pos>& a, const pair<int, pos>& b) {
-	return a.second.x < b.second.x;
+bool compx(const pos& a, const pos& b) {
+	return a.x < b.x;
 }
-bool compy(const pair<int, pos>& a, const pair<int, pos>& b) {
-	return a.second.y < b.second.y;
+bool compy(const pos& a, const pos& b) {
+	return a.y < b.y;
 }
-bool compz(const pair<int, pos>& a, const pair<int, pos>& b) {
-	return a.second.z < b.second.z;
+bool compz(const pos& a, const pos& b) {
+	return a.z < b.z;
 }
 bool comp(const weight& a, const weight& b) {
 	return a.w < b.w;
@@ -33,15 +33,15 @@ int main() {
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) parent.push_back(i);
-	vector<pair<int,pos>> v(n);
+	vector<pos> v(n);
 	vector<weight> w;
-	for (int i = 0; i < n; i++) cin >> v[i].second.x >> v[i].second.y >> v[i].second.z, v[i].first = i;
+	for (int i = 0; i < n; i++) cin >> v[i].x >> v[i].y >> v[i].z, v[i].n = i;
 	sort(v.begin(), v.end(), compx);
-	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].second.x - v[i + 1].second.x),v[i].first,v[i + 1].first });
+	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].x - v[i + 1].x),v[i].n,v[i + 1].n });
 	sort(v.begin(), v.end(), compy);
-	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].second.y - v[i + 1].second.y),v[i].first,v[i + 1].first });
+	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].y - v[i + 1].y),v[i].n,v[i + 1].n });
 	sort(v.begin(), v.end(), compz);
-	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].second.z - v[i + 1].second.z),v[i].first,v[i + 1].first });
+	for (int i = 0; i < n - 1; i++) w.push_back({ abs(v[i].z - v[i + 1].z),v[i].n,v[i + 1].n });
 	sort(w.begin(), w.end(), comp);
 	long long ans = 0;
 	for (int i = 0; i < w.size(); i++) {
