9e8ba235d3bfe5505bddc4a7d8595af8aa6f98cd
Jung <leekkzzzz@naver.com>
2025-05-03 17:18:24 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#define INF 1000000001
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}}
+using namespace std;
+int ans = 0, n;
+vector<int> v(100000);
+long long dnc(int s, int e) {
+	if (s == e) return v[s];
+	int mid = (s + e) / 2;
+	long long left = dnc(s, mid);
+	long long right = dnc(mid + 1, e);
+	int front = mid, back = mid + 1;
+	int minh = min(v[front], v[back]);
+	long long ret = minh * 2;
+	while (!(front == s && back == e)) {
+		if (front == s) back++;
+		else if (back == e) front--;
+		else if (v[back + 1] > v[front - 1]) back++;
+		else front--;
+		minh = min({ minh, v[front], v[back] });
+		ret = max(ret, (long long)minh * (back - front + 1));
+	}
+	return max({ ret,left,right });
+}
+int main() {
+	FAST_IO;
+	while (true) {
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++) cin >> v[i];
+		cout << dnc(0, n - 1) << "\n";
+	}
+}
