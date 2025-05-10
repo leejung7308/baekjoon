a715cdf77d22d5d9340dc3bd76d78ec8a9386931
Jung <leekkzzzz@naver.com>
2025-05-10 16:31:20 +0900

    [Platinum V] Title: 사탕상자, Time: 44 ms, Memory: 10216 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.cc
+++ 백준/Platinum/2243. 사탕상자/사탕상자.cc
@@ -15,14 +15,11 @@ void update(int s, int e, int n, int idx, int diff) {
 	update(mid + 1, e, n * 2 + 1, idx, diff);
 }
 int search(int s, int e, int n, int p) {
-	if (seg[n] < p) return seg[n];
-	if (s == e) return -s;
+	seg[n]--;
+	if (s == e) return s;
 	int mid = (s + e) / 2;
-	int f = search(s, mid, n * 2, p), b = 0;
-	if (f < 0) return f;
-	if (f < p) b = search(mid + 1, e, n * 2 + 1, p - f);
-	if (b < 0) return b;
-	return 0;
+	if (seg[n * 2] >= p) return search(s, mid, n * 2, p);
+	else return search(mid + 1, e, n * 2 + 1, p - seg[n * 2]);
 }
 int main() {
 	FAST_IO;
@@ -32,9 +29,7 @@ int main() {
 		int a, b, c = 0;
 		cin >> a >> b;
 		if (a == 1) {
-			int output = -search(1, 1000000, 1, b);
-			cout << output << '\n';
-			update(1, 1000000, 1, output, -1);
+			cout << search(1, 1000000, 1, b) << '\n';
 		}
 		else {
 			cin >> c;
