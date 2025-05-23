9f18fa5141958c67cb9ef5ed4a3e86e58feba407
Jung <leekkzzzz@naver.com>
2025-05-23 15:18:14 +0900

    [Gold II] Title: 저울, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#define INF 1000000001
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}}
+using namespace std;
+int main() {
+	FAST_IO;
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+	int ans = 1;
+	for (int i = 0; i < n; i++) {
+		if (v[i] > ans) break;
+		ans += v[i];
+	}
+	cout << ans;
+}
