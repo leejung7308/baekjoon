b6071366e411007e3c8a763140637ba98435016c
Jung <leekkzzzz@naver.com>
2025-07-12 14:11:19 +0900

    [Gold I] Title: 어른 상어, Time: 4 ms, Memory: 3232 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.cc
+++ 백준/Gold/19237. 어른 상어/어른 상어.cc
@@ -1,234 +1,108 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cmath>
-#include <queue>
-#include <vector>
-#include <utility>  // pair
-#include <tuple>
-#include <stack>
-// #include <map>
-#include <unordered_map>
-#define ll long long
-#define INF 1e9
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+#define INF 1000000001
+#define LLINF 9000000000000000000
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}}
+#define DIR8 const vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}}
+#define MOD 1000000007
+#define LL long long
+#define pii pair<int,int>
 using namespace std;
-
-int ans = 0;
-int n,m,k;
-int sz[21][21];
-int dir[21][21];
-pair<int,int> smell[21][21];  // <size, remaining second>
-pair<int,int> pos[401];  // shark location
-int preference[401][5][5];  // 400 sharks at max, four directions each
-int dr[] = {0,-1,1,0,0};
-int dc[] = {0,0,0,-1,1};
-
-
-void chooseCell(int i, int r, int c, pair<int,int> smell_[21][21], int sz_[21][21], int dir_[21][21]) {
-	int direction = dir[r][c];
-	int nr,nc;
-	for(int d=1;d<=4;++d) {
-		int preferredDir = preference[i][direction][d];
-		nr = r+dr[preferredDir];
-		nc = c+dc[preferredDir];
-
-		if(!(1<=nr&&nr<=n && 1<=nc&&nc<=n)) continue;
-
-		pair<int,int>& p = smell[nr][nc];
-		if(!p.first && !p.second) {
-			if(sz_[nr][nc]) {
-				if(sz[r][c] < sz_[nr][nc]) {
-					sz_[nr][nc] = sz[r][c];
-					dir_[nr][nc] = preferredDir;
-
-					pos[i].first = nr;
-					pos[i].second = nc;
-
-					smell_[nr][nc].first = sz_[nr][nc];
-					smell_[nr][nc].second = k;
-				} else {
-					pos[i].first = 0;
-					pos[i].second = 0;
-				}
-			} else {
-				sz_[nr][nc] = sz[r][c];
-				dir_[nr][nc] = preferredDir;
-
-				pos[i].first = nr;
-				pos[i].second = nc;
-				smell_[nr][nc].first = sz_[nr][nc];
-				smell_[nr][nc].second = k;
-			}
-			sz_[r][c] = 0;
-			dir_[r][c] = 0;
-
-			break;
-		}
+const pii operator+(const pii& a, const pii& b) { return { a.first + b.first,a.second + b.second }; }
+const vector<pair<int, int>> dir = { {0,0},{ -1,0 },{1,0},{0,-1},{0,1} };
+struct node { int s, t, y, x; };
+struct shark { int s, d, y, x; };
+int n, m, k, cnt = 0;
+vector<shark> sd;
+vector<bool> live;
+vector<vector<pii>> ss;
+vector<vector<int>> v;
+vector<vector<vector<int>>> pr;
+queue<node> q;
+queue<shark> sq;
+void printv() {
+	cout << "===============================\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) cout << v[i][j] << ' ';
+		cout << "       ";
+		for (int j = 0; j < n; j++) cout << '{' << ss[i][j].first << ' ' << ss[i][j].second << '}' << ' ';
+		cout << '\n';
 	}
-	return;
+	cout << "===============================\n";
 }
-
-void chooseCellMySmell(int i, int r, int c, pair<int,int> smell_[21][21], int sz_[21][21], int dir_[21][21]) {
-	int direction = dir[r][c];
-	for(int d=1;d<=4;++d) {
-		int preferredDir = preference[i][direction][d];
-		int nr = r+dr[preferredDir];
-		int nc = c+dc[preferredDir];
-
-		if(!(1<=nr&&nr<=n && 1<=nc&&nc<=n)) continue;
-
-		pair<int,int>& p = smell[nr][nc];
-		if(p.first == sz[r][c]) {  // my smell
-			if(sz_[nr][nc]) {
-				if(sz[r][c] < sz_[nr][nc]) {
-					sz_[nr][nc] = sz[r][c];
-					dir_[nr][nc] = preferredDir;
-
-					pos[i].first = nr;
-					pos[i].second = nc;
-					smell_[nr][nc].first = sz_[nr][nc];
-					smell_[nr][nc].second = k;
-				} else {
-					pos[i].first = 0;
-					pos[i].second = 0;
+int main() {
+	FAST_IO;
+	cin >> n >> m >> k;
+	sd.resize(m + 1);
+	live.resize(m + 1);
+	ss = vector<vector<pii>>(n, vector<pii>(n));
+	v = vector<vector<int>>(n, vector<int>(n));
+	pr = vector<vector<vector<int>>>(m + 1, vector<vector<int>>(5, vector<int>(4)));
+	for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) {
+		cin >> v[i][j];
+		if (v[i][j] == 0) continue;
+		cnt++;
+		live[v[i][j]] = true;
+		ss[i][j] = { v[i][j], 0 };
+		q.push({ v[i][j],0,i,j });
+		sd[v[i][j]].y = i;
+		sd[v[i][j]].x = j;
+		sd[v[i][j]].s = v[i][j];
+	}
+	for (int i = 1; i <= m; i++) cin >> sd[i].d, sq.push(sd[i]);
+	for (int i = 1; i <= m; i++) for (int j = 1; j <= 4; j++) for (int k = 0; k < 4; k++) cin >> pr[i][j][k];
+	int time;
+	for (time = 1; time <= 1000; time++) {
+		queue<shark> nq;
+		queue<node> ns;
+		vector<vector<int>> nv(n, vector<int>(n));
+		while (!sq.empty()) {
+			shark cs = sq.front();
+			sq.pop();
+			int s = cs.s, y = cs.y, x = cs.x, d = cs.d;
+			int nexty = -1, nextx = -1, nextd = -1;
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dir[pr[s][d][i]].first, nx = x + dir[pr[s][d][i]].second;
+				if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+				if (ss[ny][nx].first == 0) {
+					nexty = ny;
+					nextx = nx;
+					nextd = pr[s][d][i];
+					break;
 				}
-			} else {
-				sz_[nr][nc] = sz[r][c];
-				dir_[nr][nc] = preferredDir;
-
-				pos[i].first = nr;
-				pos[i].second = nc;
-				smell_[nr][nc].first = sz_[nr][nc];
-				smell_[nr][nc].second = k;
+				if (ss[ny][nx].first != s) continue;
+				if (nexty == -1 && nextx == -1) nexty = ny, nextx = nx, nextd = pr[s][d][i];
 			}
-			sz_[r][c] = 0;
-			dir_[r][c] = 0;
-
-			break;
-		}
-	}
-	return;
-}
-
-void sol() {
-
-	// 1. smell
-	for(int i=1;i<=n;++i) {
-		for(int j=1;j<=n;++j) {
-			if(sz[i][j]) {
-				pair<int,int>& p = smell[i][j];
-				p.first = sz[i][j];
-				p.second = k;
+			if (nv[nexty][nextx] != 0) {
+				cnt--;
+				live[s] = false;
+				continue;
+			}
+			else {
+				nv[nexty][nextx] = s;
+				nq.push({ s,nextd,nexty,nextx });
+				ns.push({ s,time,nexty,nextx });
 			}
 		}
-	}
-
-	// 2. move sharks & smell disperse
-	int sz_[21][21], dir_[21][21];
-	pair<int,int> smell_[21][21];
-	copy(&smell[0][0], &smell[0][0]+441, &smell_[0][0]);
-	copy(&sz[0][0], &sz[0][0]+441, &sz_[0][0]);
-	copy(&dir[0][0], &dir[0][0]+441, &dir_[0][0]);
-	for(int i=1;i<=m;++i) {
-		int r = pos[i].first;
-		int c = pos[i].second;
-
-		int available = 0;
-		int nextR, nextC;
-		for(int d=1;d<=4;++d) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-
-			if(!(1<=nr&&nr<=n && 1<=nc&&nc<=n)) continue;
-
-			pair<int,int>& p = smell[nr][nc];
-			if(!p.first && !p.second) {  // no smell
-				available++;
-				nextR = nr;
-				nextC = nc;
-			}
+		sq = nq;
+		v = nv;
+		while (!ns.empty()) {
+			node tmp = ns.front();
+			ss[tmp.y][tmp.x] = { tmp.s,tmp.t };
+			ns.pop();
+			q.push(tmp);
 		}
-
-		if(available >= 1) {
-			chooseCell(i,r,c,smell_,sz_,dir_);
-		} else if(!available) {
-			// my smell
-			chooseCellMySmell(i,r,c,smell_,sz_,dir_);
+		while (!q.empty() && time - k >= q.front().t) {
+			node tmp = q.front();
+			q.pop();
+			pii cur = ss[tmp.y][tmp.x];
+			if (cur.second <= time - k) ss[tmp.y][tmp.x] = { 0,0 };
 		}
-
+		//printv();
+		if (cnt == 1 && live[1]) break;
 	}
-	copy(&smell_[0][0], &smell_[0][0]+441, &smell[0][0]);
-	copy(&sz_[0][0], &sz_[0][0]+441, &sz[0][0]);
-	copy(&dir_[0][0], &dir_[0][0]+441, &dir[0][0]);
-
-	for(int i=1;i<=m;++i) {
-		int r = pos[i].first;
-		int c = pos[i].second;
-
-		for(int j=1;j<=n;++j) {
-			for(int k=1;k<=n;++k) {
-				if(j==r && k==c) continue;
-				if(!smell[j][k].second) continue;
-				if(smell[j][k].first == i) {
-					if(--smell[j][k].second == 0) {
-						smell[j][k].first = 0;
-					}
-				}
-			}
-		}
-	}
-
-	return;
-}
-
-int main(void) {
-	// ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	scanf("%d%d%d", &n, &m, &k);
-	for(int i=1;i<=n;++i) {
-		for(int j=1;j<=n;++j) {
-			scanf("%d", &sz[i][j]);
-			if(sz[i][j]) {
-				int size = sz[i][j];
-				pos[size].first = i;
-				pos[size].second = j;
-			}
-		}
-	}
-	for(int i=1;i<=m;++i) {
-		int r,c;
-		r = pos[i].first;
-		c = pos[i].second;
-
-		scanf("%d", &dir[r][c]);
-	}
-
-
-	for(int i=1;i<=m;++i) {
-		for(int j=1;j<=4;++j) {
-			for(int k=1;k<=4;++k) {
-				scanf("%d", &preference[i][j][k]);
-			}
-		}
-	}
-
-	while(ans <= 1000) {
-		sol();
-		ans++;
-
-		int finish = 1;
-		for(int j=2;j<=m;++j) {
-			if(pos[j].first || pos[j].second) {
-				finish = 0;
-			}
-		}
-
-		if(finish) {
-			break;
-		}
-	}
-
-	ans = ans <= 1000 ? ans : -1;
-	printf("%d\n", ans);
-	
-	return 0;
+	if (cnt > 1 || live[1] == false) cout << -1;
+	else cout << time;
 }
