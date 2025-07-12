f4211cc5682eff7e3740c0c22ce23e6ac60b23ac
Jung <leekkzzzz@naver.com>
2025-07-12 16:51:00 +0900

    [Platinum IV] Title: 달리기, Time: 176 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.cc
+++ 백준/Platinum/2517. 달리기/달리기.cc
@@ -14,32 +14,33 @@ const pii operator+(const pii& a, const pii& b) { return { a.first + b.first,a.s
 bool cmp(const pii& a, const pii& b) { return a.first > b.first; }
 int n;
 vector<pii> v;
-int seg[2000000];
-void update(int start, int end, int n, int idx) {
-	if (idx < start || end < idx) return;
-	seg[n]++;
-	if (start == end) return;
-	int mid = (start + end) / 2;
-	update(start, mid, n * 2, idx);
-	update(mid + 1, end, n * 2 + 1, idx);
+vector<int> fen;
+void update(int idx) {
+	while (idx <= n) {
+		fen[idx]++;
+		idx += idx & -idx;
+	}
 }
-int sum(int start, int end, int n, int left, int right) {
-	if (end < left || right < start) return 0;
-	if (left <= start && end <= right) return seg[n];
-	int mid = (start + end) / 2;
-	return sum(start, mid, n * 2, left, right) + sum(mid + 1, end, n * 2 + 1, left, right);
+int sum(int idx) {
+	int ret = 0;
+	while (idx >= 1) {
+		ret += fen[idx];
+		idx -= idx & -idx;
+	}
+	return ret;
 }
 int main() {
 	FAST_IO;
 	cin >> n;
+	fen.resize(n + 1);
 	vector<int> ans(n + 1);
 	v.resize(n + 1);
 	for (int i = 1; i <= n; i++) v[i].second = i, cin >> v[i].first;
 	sort(v.begin() + 1, v.end());
 	for (int i = 1; i <= n; i++) {
-		int tmp = sum(1, n, 1, 1, v[i].second - 1);
+		int tmp = sum(v[i].second - 1);
 		ans[v[i].second] = v[i].second - tmp;
-		update(1, n, 1, v[i].second);
+		update(v[i].second);
 	}
 	for (int i = 1; i <= n; i++) cout << ans[i] << '\n';
 }
