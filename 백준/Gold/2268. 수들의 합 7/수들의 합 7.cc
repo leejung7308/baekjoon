de4e86aa18c9c9c3b9406d340249ad99547f93d5
Jung <leekkzzzz@naver.com>
2025-07-23 12:30:25 +0900

    [Gold I] Title: 수들의 합 7, Time: 696 ms, Memory: 41088 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#define INF 1000000001
+#define LLINF 9000000000000000000
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+#define DIR8 const vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+#define MOD 1000000007
+#define LL long long
+#define pii pair<int, int>
+using namespace std;
+const pii operator+(const pii& a, const pii& b) { return { a.first + b.first, a.second + b.second }; }
+vector<long long> v, seg;
+int n, m;
+void update(int start,int end, int n, int idx, int diff) {
+    if (idx < start || end < idx) return;
+    seg[n] += diff;
+    if (start == end) return;
+    int mid = (start + end) / 2;
+    update(start, mid, n * 2, idx, diff);
+    update(mid + 1, end, n * 2 + 1, idx, diff);
+}
+long long sum(int start,int end, int n, int left, int right) {
+    if (end < left || right < start) return 0;
+    if (left <= start && end <= right) return seg[n];
+    int mid = (start + end) / 2;
+    return sum(start, mid, n * 2, left, right) + sum(mid + 1, end, n * 2 + 1, left, right);
+}
+int main() {
+    FAST_IO;
+    cin >> n >> m;
+    v.resize(n + 1);
+    seg.resize(n * 4);
+    while (m--) {
+        int op, a, b;
+        cin >> op >> a >> b;
+        if (op == 0) {
+            if (a > b) swap(a, b);
+            cout << sum(1, n, 1, a, b) << '\n';
+        }
+        else {
+            long long diff = b - v[a];
+            update(1, n, 1, a, diff);
+            v[a] = b;
+        }
+    }
+}
