395d87bbcd46ff1a061f82768d203d7712a5b93a
Jung <leekkzzzz@naver.com>
2025-07-26 14:26:44 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    cout << "2000\n";
+    for (int i = 0; i < 1000; i++) cout << "1 ";
+    for (int i = 0; i < 1000; i++) cout << "1000 ";
+}
