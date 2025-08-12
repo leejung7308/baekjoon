c9907a9b612c2fdb3d6eeb650c58c7c81418daf7
Jung <leekkzzzz@naver.com>
2025-08-12 13:21:48 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+#define INF 1000000001
+#define LLINF 9000000000000000000
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}
+#define DIR8 const vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+#define MOD 1000000007
+#define LL long long
+#define pii pair<int, int>
+using namespace std;
+const pii operator+(const pii& a, const pii& b) { return { a.first + b.first, a.second + b.second }; }
+
+int main() {
+    FAST_IO;
+    string n;
+    cin >> n;
+    sort(n.rbegin(), n.rend());
+    cout << n;
+}
