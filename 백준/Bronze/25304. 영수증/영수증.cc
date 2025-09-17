662407f30fa4b705b218ba4b91764b37295f3f0c
Jung <leekkzzzz@naver.com>
2025-09-17 17:56:58 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define INF 1000000001
+#define LLINF 9000000000000000000
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}
+#define DIR8 const vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+#define MOD 1000000007
+#define LL long long
+#define pii pair<int, int>
+#define MAX 1000000
+using namespace std;
+const pii operator+(const pii& a, const pii& b) { return { a.first + b.first, a.second + b.second }; }
+
+int main() {
+    FAST_IO;
+    int x;
+    cin >> x;
+    int n;
+    cin >> n;
+    int sum = 0;
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+        sum += a * b;
+    }
+    if (sum == x) cout << "Yes\n";
+	else cout << "No\n";
+}
