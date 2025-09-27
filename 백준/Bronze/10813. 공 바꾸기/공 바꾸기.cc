ad85b5e4562ed9fdb0ffb123e8dab31188e52394
Jung <leekkzzzz@naver.com>
2025-09-27 13:04:02 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define INF 1000000001
+#define LLINF 9000000000000000000
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define DIR const vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}}
+#define DIR8 const vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+#define MOD 1000000007
+#define LL long long
+#define pii pair<int, int>
+#define MAX 1000000
+using namespace std;
+const pii operator+(const pii& a, const pii& b) { return { a.first + b.first, a.second + b.second }; }
+
+int main() {
+    FAST_IO;
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++) v[i] = i;
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+		swap(v[a], v[b]);
+    }
+	for (int i = 1; i <= n; i++) cout << v[i] << ' ';
+}
