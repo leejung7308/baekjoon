768846929f7b6628bda24cc0a018056ca13aa6d9
Jung <leekkzzzz@naver.com>
2025-10-24 15:27:27 +0900

    [Platinum IV] Title: 트리, Time: 148 ms, Memory: 13768 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13306. 트리/트리.cc
+++ 백준/Platinum/13306. 트리/트리.cc
@@ -12,31 +12,45 @@
 using namespace std;
 const pii operator+(const pii& a, const pii& b) { return { a.first + b.first, a.second + b.second }; }
 vector<int> parent;
-int findp(int x) {
+vector<int> v;
+struct node {
+    int x, a, b;
+};
+int findroot(int x) {
     if (x == parent[x]) return x;
-    return findp(parent[x]);
+    return parent[x] = findroot(parent[x]);
 }
 int main() {
     FAST_IO;
     int n, q;
     cin >> n >> q;
+    v.resize(n + 1);
     parent.resize(n + 1);
-    parent[1] = 1;
-    for (int i = 2; i <= n; i++) cin >> parent[i];
-    while (q > 0) {
+    stack<node> st;
+    stack<string> ans;
+    for (int i = 1; i <= n; i++) parent[i] = i;
+    for (int i = 2; i <= n; i++) cin >> v[i];
+    q += n - 1;
+    while (q--) {
         int x;
         cin >> x;
         if (x == 0) {
             int b;
             cin >> b;
-            parent[b] = b;
+            st.push({ x,b,v[b] });
         }
         else {
-            q--;
             int c, d;
             cin >> c >> d;
-            if (findp(c) == findp(d)) cout << "YES\n";
-            else cout << "NO\n";
+            st.push({ x,c,d });
         }
     }
+    while (!st.empty()) {
+        int x = st.top().x, a = st.top().a, b = st.top().b;
+        st.pop();
+        if (x == 0) parent[a] = b;
+        else if (findroot(a) == findroot(b)) ans.push("YES\n");
+        else ans.push("NO\n");
+    }
+    while (!ans.empty()) cout << ans.top(), ans.pop();
 }
